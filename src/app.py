8d4185ee9769d3817311c7cd374f9cc6a7380129
Iapetus-11 <Iapetus-11@users.noreply.github.com>
2022-12-04 04:26:13 +0000

    format / fix code

--- src/app.py
+++ src/app.py
@@ -18,7 +18,7 @@ from src.models import SlackEmoji, SlackMessage, SlackMessageEmojiUse, SlackUser
 from src.utils.slack import get_block_emojis
 
 logging.basicConfig(level=getattr(logging, CONFIG.LOG_LEVEL))
-logger = logging.getLogger('app')
+logger = logging.getLogger("app")
 
 app = AsyncApp(
     token=CONFIG.SLACK_BOT_TOKEN,
@@ -42,24 +42,25 @@ api = FastAPI(
 @app.event("message")
 async def app_handle_message(event: dict[str, Any]):
     db_message = await SlackMessage.create(
-        id=event['client_msg_id'],
-        user_id=event['user'],
-        channel_id=event['channel'],
-        char_count=len(event['text']),
-        timestamp=event['ts'],
+        id=event["client_msg_id"],
+        user_id=event["user"],
+        channel_id=event["channel"],
+        char_count=len(event["text"]),
+        timestamp=event["ts"],
     )
 
-    for emoji, count in Counter(get_block_emojis(event.get('blocks', []))).items():
+    for emoji, count in Counter(get_block_emojis(event.get("blocks", []))).items():
         await SlackMessageEmojiUse.create(message=db_message, emoji_id=emoji, count=count)
 
 
 @app.event("reaction_added")
 async def app_handle_reaction_added(event: dict[str, Any]):
-    if event['item']['type'] != 'message':
+    if event["item"]["type"] != "message":
         return
 
     user_reactions, _ = await SlackUserEmojiReactions.get_or_create(
-        emoji_id=event['reaction'], user_id=event['user'], defaults={'count': 0})
+        emoji_id=event["reaction"], user_id=event["user"], defaults={"count": 0}
+    )
 
     user_reactions.count += 1
     await user_reactions.save()
@@ -67,10 +68,12 @@ async def app_handle_reaction_added(event: dict[str, Any]):
 
 @app.event("reaction_removed")
 async def app_handle_reaction_removed(event: dict[str, Any]):
-    if event['item']['type'] != 'message':
+    if event["item"]["type"] != "message":
         return
 
-    if user_reactions := await SlackUserEmojiReactions.get_or_none(emoji_id=event['reaction'], user_id=event['user']):
+    if user_reactions := await SlackUserEmojiReactions.get_or_none(
+        emoji_id=event["reaction"], user_id=event["user"]
+    ):
         if user_reactions.count <= 1:
             await user_reactions.delete()
         else:
@@ -78,43 +81,43 @@ async def app_handle_reaction_removed(event: dict[str, Any]):
             await user_reactions.save()
 
 
-@api.get('/')
+@api.get("/")
 async def api_home():
     return {
         "made_by": {
             "name": "Milo",
             "url": "https://iapetus11.me",
         },
-        "source": "https://github.com/Iapetus-11/slack-leaderboard"
+        "source": "https://github.com/Iapetus-11/slack-leaderboard",
     }
 
 
-@api.get('/emojis/')
+@api.get("/emojis/")
 async def api_emojis():
     return {emoji.id: emoji.url for emoji in await SlackEmoji.all()}
 
 
-@api.get('/emojis/leaderboard/')
+@api.get("/emojis/leaderboard/")
 async def api_emojis_leaderboards(since: datetime = Query(None)):
     if not since:
         since = datetime.utcnow() - timedelta(days=7)
 
     data = {
-        r['emoji_id']: r['uses']
+        r["emoji_id"]: r["uses"]
         for r in (
-            await SlackMessageEmojiUse
-            .filter(created_at__gt=since)
-            .group_by('emoji_id')
-            .annotate(uses=Sum(F('count')))
-            .values('emoji_id', 'uses')
+            await SlackMessageEmojiUse.filter(created_at__gt=since)
+            .group_by("emoji_id")
+            .annotate(uses=Sum(F("count")))
+            .values("emoji_id", "uses")
         )
     }
 
-    data.update({
-        r['emoji_id']: r['count']
-        for r in
-        await SlackUserEmojiReactions.all().values('emoji_id', 'count')
-    })
+    data.update(
+        {
+            r["emoji_id"]: r["count"]
+            for r in await SlackUserEmojiReactions.all().values("emoji_id", "count")
+        }
+    )
 
     data = dict(sorted(data.items(), key=(lambda kv: kv[1]), reverse=True))
 
@@ -127,23 +130,27 @@ async def sync_emojis():
     emojis = dict[str, SlackEmoji]()
     aliases = dict[str, str]()
 
-    for name, url in response.data['emoji'].items():
+    for name, url in response.data["emoji"].items():
         # There are emoji "aliases", we handle those later
-        if 'alias:' in url:
-            aliases[name] = url.split(':')[1]
+        if "alias:" in url:
+            aliases[name] = url.split(":")[1]
             continue
 
-        emoji_hash = url.split('/')[-1].split('.')[0]
+        emoji_hash = url.split("/")[-1].split(".")[0]
 
-        emojis[name], _ = await SlackEmoji.get_or_create(id=name, defaults={'hash': emoji_hash, 'url': url})
+        emojis[name], _ = await SlackEmoji.get_or_create(
+            id=name, defaults={"hash": emoji_hash, "url": url}
+        )
 
     # We treat emoji aliases as their own emojis in our db
     for alias_name, name in aliases.items():
         if emoji := emojis.get(name):
-            await SlackEmoji.get_or_create(id=alias_name, defaults={'hash': emoji.hash, 'url': emoji.url})
+            await SlackEmoji.get_or_create(
+                id=alias_name, defaults={"hash": emoji.hash, "url": emoji.url}
+            )
 
 
-@api.on_event('startup')
+@api.on_event("startup")
 async def api_handle_startup():
     await Tortoise.init(TORTOISE_ORM)
 
@@ -152,10 +159,15 @@ async def api_handle_startup():
     await sync_emojis()
 
 
-@api.on_event('shutdown')
+@api.on_event("shutdown")
 async def api_handle_shutdown():
     await app_handler.close_async()
 
 
 if __name__ == "__main__":
-    uvicorn.run('src.app:api', host=CONFIG.API_HOST, port=CONFIG.API_PORT, log_level=CONFIG.LOG_LEVEL.lower())
+    uvicorn.run(
+        "src.app:api",
+        host=CONFIG.API_HOST,
+        port=CONFIG.API_PORT,
+        log_level=CONFIG.LOG_LEVEL.lower(),
+    )
--- src/utils/slack.py
+++ src/utils/slack.py
@@ -4,8 +4,8 @@ from typing import Any, Generator
 def get_block_emojis(elements: list[dict[str, Any]]) -> Generator[str, None, None]:
     for el in elements:
         # Ensure block element is an emoji and is a custom one
-        if el['type'] == 'emoji' and not el.get('unicode'):
-            yield el['name']
+        if el["type"] == "emoji" and not el.get("unicode"):
+            yield el["name"]
 
-        if sub_elements := el.get('elements'):
+        if sub_elements := el.get("elements"):
             yield from get_block_emojis(sub_elements)
