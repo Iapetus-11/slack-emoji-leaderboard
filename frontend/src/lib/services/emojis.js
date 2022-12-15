55616f1d5eb5886cbc30ce9ae55ae93d0bbe4970
Milo Weinberg <iapetus011@gmail.com>
2022-12-15 08:42:23 -0500

    Test

--- frontend/src/lib/services/emojis.js
+++ frontend/src/lib/services/emojis.js
@@ -1,16 +1,16 @@
-import { env } from '$env/dynamic/public';
+import { PUBLIC_API_ADDRESS, PUBLIC_API_AUTH } from '$env/static/public';
 
 /**
  * @returns {Promise<{ emojis: Object<string, Object> }>}
  */
 export const fetchEmojis = () => {
-  return fetch(`${env.PUBLIC_API_ADDRESS}/emojis/`, {
-    headers: { Authorization: env.PUBLIC_API_AUTH },
+  return fetch(`${PUBLIC_API_ADDRESS}/emojis/`, {
+    headers: { Authorization: PUBLIC_API_AUTH },
   }).then((response) => response.json());
 };
 
 export const fetchEmojiLeaderboard = () => {
-  return fetch(`${env.PUBLIC_API_ADDRESS}/emojis/leaderboard/`, {
-    headers: { Authorization: env.PUBLIC_API_AUTH },
+  return fetch(`${PUBLIC_API_ADDRESS}/emojis/leaderboard/`, {
+    headers: { Authorization: PUBLIC_API_AUTH },
   }).then((response) => response.json());
 };
