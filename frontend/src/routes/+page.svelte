bc66fd335e56a8bfd515e7b926ccfb9685f47c1e
Milo Weinberg <iapetus011@gmail.com>
2022-12-17 11:38:58 -0500

    Extract leaderboard into its own component

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -29,16 +29,7 @@
 </svelte:head>
 
 <div class="flex flex-col md:h-screen justify-center items-center my-3 mx-3 space-y-5">
-  <table class="flex flex-col space-y-3 w-full sm:w-4/5 md:w-3/5 lg:w-2/5">
-    {#each [...Object.entries(leaderboard).entries()].slice(0, 10) as [idx, [emoji, uses]] (emoji)}
-      <LeaderboardItem
-        names={[emoji, ...emojis[emoji].aliases]}
-        iconUrl={emojis[emoji].url}
-        position={idx}
-        {uses}
-      />
-    {/each}
-  </table>
+  <Leaderboard {emojis} {leaderboard} limit="{data.limit}" />
 
   <p class="text-gray-500 text-center text-xs mx-5 w-full sm:w-4/5 md:w-3/5 lg:w-2/5">
     This leaderboard is for the past one week of activity (rolling). Both an emoji in a message and
