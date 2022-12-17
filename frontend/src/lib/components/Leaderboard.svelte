416c81fed86d98441650b80a4c0b1ee6160c34b5
Iapetus-11 <Iapetus-11@users.noreply.github.com>
2022-12-17 16:40:16 +0000

    format / fix code

--- frontend/src/lib/components/Leaderboard.svelte
+++ frontend/src/lib/components/Leaderboard.svelte
@@ -1,19 +1,19 @@
 <script>
-    import LeaderboardItem from "$lib/components/LeaderboardItem.svelte";
+  import LeaderboardItem from '$lib/components/LeaderboardItem.svelte';
 
-    export let emojis;
-    export let leaderboard;
-    export let limit = 10;
-    export let offset = 0;
+  export let emojis;
+  export let leaderboard;
+  export let limit = 10;
+  export let offset = 0;
 </script>
 
 <table class="flex flex-col space-y-3 w-full sm:w-4/5 md:w-3/5 lg:w-2/5">
-{#each [...Object.entries(leaderboard).entries()].slice(offset, limit) as [idx, [emoji, uses]] (emoji)}
-  <LeaderboardItem
-    names={[emoji, ...emojis[emoji].aliases]}
-    iconUrl={emojis[emoji].url}
-    position={idx}
-    {uses}
-  />
-{/each}
+  {#each [...Object.entries(leaderboard).entries()].slice(offset, limit) as [idx, [emoji, uses]] (emoji)}
+    <LeaderboardItem
+      names={[emoji, ...emojis[emoji].aliases]}
+      iconUrl={emojis[emoji].url}
+      position={idx}
+      {uses}
+    />
+  {/each}
 </table>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <script>
   import { onDestroy, onMount } from 'svelte';
   import { fetchEmojiLeaderboard, fetchEmojis } from '$lib/services/emojis.js';
-  import Leaderboard from "$lib/components/Leaderboard.svelte";
+  import Leaderboard from '$lib/components/Leaderboard.svelte';
 
-  export let data
+  export let data;
 
   let emojis = {};
   let leaderboard = {};
@@ -23,15 +23,11 @@
 
 <svelte:head>
   <title>Emoji Leaderboard</title>
-  <link
-    rel="icon"
-    type="image/x-icon"
-    href="{emojis[Object.entries(leaderboard)[0]?.[0]]?.url}"
-  />
+  <link rel="icon" type="image/x-icon" href={emojis[Object.entries(leaderboard)[0]?.[0]]?.url} />
 </svelte:head>
 
 <div class="flex flex-col md:h-screen justify-center items-center my-3 mx-3 space-y-5">
-  <Leaderboard {emojis} {leaderboard} limit="{data.limit}" />
+  <Leaderboard {emojis} {leaderboard} limit={data.limit} />
 
   <p class="text-gray-500 text-center text-xs mx-5 w-full sm:w-4/5 md:w-3/5 lg:w-2/5">
     This leaderboard is for the past one week of activity (rolling). Both an emoji in a message and
