145146b459b50589c4c4d3dc2b8b40521d86b292
Milo Weinberg <iapetus011@gmail.com>
2023-04-17 12:59:21 -0400

    Revert "Show top 24 by default"
    This reverts commit 23abe796502c36b2afe822cba8820fe0507e7ec8.

--- frontend/src/routes/+page.js
+++ frontend/src/routes/+page.js
@@ -1,6 +1,6 @@
 export async function load({ url }) {
   return {
-    limit: parseInt(url.searchParams.get('limit')) || 24,
+    limit: parseInt(url.searchParams.get('limit')) || 10,
     unique: (url.searchParams.get('unique') || '').toLowerCase() !== 'false',
   };
 }
