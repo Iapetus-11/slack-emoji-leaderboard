23abe796502c36b2afe822cba8820fe0507e7ec8
Milo Weinberg <iapetus011@gmail.com>
2023-04-17 12:52:22 -0400

    Show top 24 by default

--- frontend/src/routes/+page.js
+++ frontend/src/routes/+page.js
@@ -1,6 +1,6 @@
 export async function load({ url }) {
   return {
-    limit: parseInt(url.searchParams.get('limit')) || 10,
+    limit: parseInt(url.searchParams.get('limit')) || 24,
     unique: (url.searchParams.get('unique') || '').toLowerCase() !== 'false',
   };
 }
